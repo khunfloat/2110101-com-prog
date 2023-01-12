0b63540de56f3ec30b0e391938a7fe3925b44dde
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-01-12 09:58:16 +0700

    feat: update new contents

--- test.py
+++
@@ -1,8 +0,0 @@
-from datetime import datetime, timezone, timedelta
-
-tz = timezone(timedelta(hours = 7))
-date = datetime.now(tz=tz)
-date_el = date.ctime().split(' ')
-
-time_formated = f"{date_el[2]} {date_el[1]} {date_el[4]} {date_el[3]}"
-print(time_formated)
