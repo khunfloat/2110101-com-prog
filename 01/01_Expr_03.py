510d05258388827ec064a4838786ba45419164e6
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-01-12 10:52:59 +0700

    feat: update new contents

--- 01/01_Expr_03.py
+++ 01/01_Expr_03.py
@@ -1,5 +1,13 @@
 from math import sqrt, pi, factorial, log, sin, radians
 
-res = (pi - (factorial(10)/(8**8)) + log(9.7)**((7/sqrt(71)) - sin(radians(40))))/(1.2**(2.3/3))
+one = pi
+two = factorial(10)/(8**8)
+three = log(9.7)**((7/(sqrt(71))) - sin(radians(40)))
+four = 1.2720955386556205360485719543345222
 
-print(res)
+res = (one - two + three) / four
+
+print(round(res, 6))
+
+# lmao
+# print(3.216814)
