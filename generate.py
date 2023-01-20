06e41dd078d7e25166f447a5c819e6d4d6e59d63
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-01-20 10:50:50 +0700

    feat: update new contents

--- generate.py
+++ generate.py
@@ -7,7 +7,6 @@ file_list = listdir(f'./{directory}')
 pdf_list = []
 for file in file_list:
     if not exists(f'./{directory}/{file.split(".")[0]}.py'):
-        # pdf_list.append(file.split('.')[0])
         with open(f'./{directory}/{file.split(".")[0]}.py', 'w') as f:
             f.close()
 
