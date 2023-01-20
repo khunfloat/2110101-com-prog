911621282dcd4be7a1f76cce42141f83566e7367
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-01-20 10:53:38 +0700

    feat: update new contents

--- runner.py
+++ runner.py
@@ -9,6 +9,8 @@ time_formated = f"{date_el[2]} {date_el[1]} {date_el[4]} {date_el[3]}"
 root_dir = os.listdir()
 expr_dir = list(filter(lambda dir: dir.startswith('0'), root_dir))
 
+print(expr_dir)
+
 recent_file = ""
 
 for dir in expr_dir:
