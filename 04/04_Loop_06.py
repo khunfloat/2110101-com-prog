8c4978f1e703ed034cd96c2e51f720072fe33be0
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-02-19 15:48:37 +0700

    📕 feat: update new contents

--- 04/04_Loop_06.py
+++ 04/04_Loop_06.py
@@ -18,15 +18,6 @@ for i in range(h-2):
     print('.'*(h-2-i) + '*' + '.'*(2*(i+1)-1) + '*')
 print('*'*(2*h-1))
 
-
-
-
-
-
-
-
-
-
 # h = int(input()) 
 # print('.'*(h-1)+'*')
 # for i in range(1, h-1):
