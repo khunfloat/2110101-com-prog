a7e095da90c5f0981a921acec3e9505b876f30fa
khunfloat <60844731+khunfloat@users.noreply.github.com>
2023-02-23 15:05:22 +0700

    📕 feat: update new contents

---
+++ Q1/practice/2565_1_Quiz_1_3_1.py
@@ -0,0 +1,7 @@
+# print(sum([int(e) for e in input().replace('-', '+-').split('+') if e != '']))
+
+# print(input().replace('-', '+-').split('+'))
+
+# print(sum(map(int, input().replace('-', '0+-').split('+'))))
+
+# print(input().replace('-', '0+-').split('+'))
